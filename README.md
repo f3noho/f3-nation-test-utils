4a24ed749b8f98d993a83ed0076c412ed34c84b3
Manuel Lopez <jmlopez.rod@gmail.com>
2025-08-23 12:09:53 -0500

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# f3-nation-test-utils
+
+A collection of utilities to assist with testing in the F3 Nation ecosystem.
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+from f3_nation_test_utils.version import __version__
+
+
+def test_version():
+    assert __version__ == '0.0.0.dev0'
